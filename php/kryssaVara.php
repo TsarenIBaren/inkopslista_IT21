e8d778d2783e62ea58c496846b0b6481b5c4c22b
Tsaren <117976226+TsarenIBaren@users.noreply.github.com>
2023-05-16 11:08:25 +0300

    Spara vara och grundläggande tester funkar

--- php/kryssaVara.php
+++ php/kryssaVara.php
@@ -1,2 +1,12 @@
 <?php
 declare (strict_types=1);
+
+// Läs in gemensamma funktioner
+
+// Läs och kontrollera indata
+
+// Koppla mot databasen
+
+// toggla checked-värdet
+
+// Skicka svar
--- php/sparaVara.php
+++ php/sparaVara.php
@@ -1,2 +1,35 @@
 <?php
 declare (strict_types=1);
+
+// Inkludera gemensamma
+require_once "funktioner.php";
+
+//läs indata och sanera
+// Kontrollera metod
+if ($_SERVER['REQUEST_METHOD']!=='POST') {
+    $error=new stdClass();
+    $error->meddelande=["Wrong method", "Sidan ska anropas med POST"];
+    skickaJSON($error, 405);
+}
+
+// Läs indata
+$vara=filter_input(INPUT_POST, 'vara', FILTER_SANITIZE_SPECIAL_CHARS);
+if(!isset($vara) || mb_strlen($vara)>50) {
+    $error=new stdClass();
+    $error->meddelande=["bad input", "Parametern 'vara' saknas eller är för lång (max 50 tecken)"];
+    skickaJSON($error, 400);
+}
+
+//koppla mot databas
+$db=connectDB();
+
+//Spara data
+$sql="INSERT INTO varor (namn) VALUES (:vara)";
+$stmt=$db->prepare($sql);
+
+$db->execute(['vara'=>$vara]);
+$id=$db->lastInsertId();
+
+
+// Skicka tillbaka svar
+skickaJSON(['id'=>$id]);
