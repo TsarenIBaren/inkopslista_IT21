64dc220e3d54323cdb65c00f70fca9a6beaf3264
Tsaren <117976226+TsarenIBaren@users.noreply.github.com>
2023-05-22 14:22:29 +0300

    test för hämta alla varor funkar på testsidan. Tog bort exit

--- test/testHamtaAlla.php
+++ test/testHamtaAlla.php
@@ -2,12 +2,39 @@
 declare(strict_types=1);
 
 try {
+    // Skapa ett "handle" till curl för att läsa svaret från angivet sida
+    $ch=curl_init('http://localhost/inkopslista/php/hamtaAlla.php');
+    //Return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // Testa!
+    testOK();
 
 } catch (Exception $e) {
     echo "<p class='error'>";
     echo "Något gick Jättefel!<br>";
     echo $e->getMessage();
     echo "</p>";
+} finally {
+    // Stäng "Handle" för att frigöra resurser
+    curl_close($ch);
 }
 
-exit;
+function testOK($curlHandle);
+{
+    // Anropar hämta alla och sparar svaret i en variabel
+    $svarJSON= include "../php/hamtaAlla.php";
+
+    // Gör om till objekt
+    $svar = json_decode($svarJSON);
+    if(is_array($svar)) {   // Svaret är en array
+        if(count($svar)>0) {    // Det finns radera
+            echo "<p class='ok'>Hämta alla OK, " .count($svar) . "rader returnerades</p>"
+        } else {
+            echo "<p class='ok'>Hämta alla OK, inga radera fanns</p>";
+        }
+
+    } else {
+        echo "<p class='error'>Hämta alla misslyckades</p>";
+    }
+}
