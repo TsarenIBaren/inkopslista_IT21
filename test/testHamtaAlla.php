e09a948f2629969a3e91946174d8c732d54817bb
Tsaren <117976226+TsarenIBaren@users.noreply.github.com>
2023-05-22 13:17:56 +0300

    lagt till ram i alla testfiler

---
+++ test/testHamtaAlla.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+try {
+
+} catch (Exception $e) {
+    echo "<p class='error'>";
+    echo "Något gick Jättefel!<br>";
+    echo $e->getMessage();
+    echo "</p>";
+}
+
+exit;
